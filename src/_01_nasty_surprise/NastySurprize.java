04d81392c60468c6dfe85a9c9fa0a0b3436cac41
BEEP <beep@beep.com>
2020-03-02 18:25:17 -0800

    yoi

--- src/_01_nasty_surprise/NastySurprize.java
+++ src/_01_nasty_surprise/NastySurprize.java
@@ -1,5 +1,7 @@
-package _01_nasty_surprise;
+   package _01_nasty_surprise;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -10,20 +12,55 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class NastySurprize {
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
+public class NastySurprize implements ActionListener {
 JFrame f = new JFrame();
 JPanel p = new JPanel();
-JButton b = new JButton("hi");
-JButton u = new JButton();
+JButton b = new JButton("Trick");
+JButton u = new JButton("Treat");
 	void show() {
+	b.addActionListener(this);
+	u.addActionListener(this);
 		f.setVisible(true);
 		f.add(p);
        p.add(b);
        p.add(u);
 f.pack();
+	
+	 {
+		
 	}
 	
 	
+	
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource()==u) {
+			showPictureFromTheInternet("https://i.pinimg.com/236x/76/97/a0/7697a0b6872b4d965225c2de2d6e0919--demotivational-posters-funny-photos.jpg");
+		}
+		
+		if (e.getSource()==b) {
+			showPictureFromTheInternet("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTXFbUhBlQ10hp_uHrp5cuFvMJfQVCPxfhmVjp-c27J0r2wvHIC");
+		}
+	
+	}
+	
+	private void showPictureFromTheInternet(String imageUrl) {
+	    try {
+	        URL url = new URL(imageUrl);
+	        Icon icon = new ImageIcon(url);
+	        JLabel imageLabel = new JLabel(icon);
+	        JFrame frame = new JFrame();
+	        frame.add(imageLabel);
+	        frame.setVisible(true);
+	        frame.pack();
+	    } catch (MalformedURLException e) {
+	        e.printStackTrace();
+	    }
+	}
 	
 
 	
--- src/_02_simon_says/SimonSays.java
+++ src/_02_simon_says/SimonSays.java
@@ -30,17 +30,20 @@ public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
-
+	JFrame f= new JFrame();
 	public void run() {
 		// 2. Add the four images that match keyboard keys like this:
 		// images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
-
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_DOWN), "down.jpg");
+		images.put(new Integer(KeyEvent.VK_LEFT), "left.jpg");
+		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpg");
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching
 		// key when
 		// 'Simon says' otherwise press a different key"
-
+		JOptionPane.showMessageDialog(null, "Press matching key when Simon says.");
 		// 4. Call the showImage method to show an image
-
+		showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -74,23 +77,22 @@ public class SimonSays extends KeyAdapter {
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
-
 		// 6. Set the frame to visible
-
+		f.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		// frame.add(getNextRandomImage());
+		 f.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
-
+		 f.setName("Joe");
 		// 9. Pack the frame
-
+		 f.pack();
 		// 10. Set the defaultCloseOperation of your frame to
 		// JFrame.EXIT_ON_CLOSE
-
+		 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+		 f.addKeyListener(null);
 		// 12. Create a new instance of Random
-
+		 
 		// 13. Use the Random and the speak method to either say
 		// "Simon says press this key" or "Press this key"
 

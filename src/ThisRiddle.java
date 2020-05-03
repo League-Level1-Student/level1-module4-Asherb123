b4f727b87dd44720ecbd478eefc35e76ee29dd8a
Asherb123 <46113623+Asherb123@users.noreply.github.com>
2020-05-03 00:56:08 -0700

    YAY

--- src/ThisRiddle.java
+++ src/ThisRiddle.java
@@ -9,72 +9,72 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class ThisRiddle implements MouseListener {
-	JFrame frame= new JFrame();
+	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	
+
 	JButton check = new JButton("Check!");
 	JButton hint = new JButton("Hint");
-	
-	JLabel riddle = new JLabel("If a guy has 3 dollars, and he spends 2 of them, how much dough does he have left?");
+
+	JLabel riddle = new JLabel("If a guy has a Dr. Pepper, and he drinks it, does he still have something to drink?");
 	JTextField answer = new JTextField(10);
-	
-	
+
 	void Riddles() {
 		frame.add(panel);
 		panel.add(riddle);
 		panel.add(answer);
 		panel.add(hint);
 		panel.add(check);
-		
-	hint.addMouseListener(this);
-	check.addMouseListener(this);	
-		
-		JTextField s = answer = new JTextField();
-		
+
+		hint.addMouseListener(this);
+		check.addMouseListener(this);
+
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		frame.pack();
 	}
 
-
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource()==hint) {
-			JOptionPane.showMessageDialog(null, "If you need this hint you are truly stupid. What is 3-2?");
-		}
-		
-	}
+		String i = answer.getText();
 
+		if (e.getSource() == hint) {
+			JOptionPane.showMessageDialog(null, "Come on dude. You don't need  hint.");
+		}
+		if (e.getSource() == check && i.equals("no")) {
+
+			JOptionPane.showMessageDialog(null, "Holy creeper. You are a master.");
+
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, "Look at this duuuude");
+
+		}
+	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
-	
-	
+
 }
